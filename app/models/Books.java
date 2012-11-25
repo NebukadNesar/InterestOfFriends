9939d6f518244ef722a62823ac8b0632c810ae7f
Burhan Cerit <burhancerit@gmail.com>
2012-11-25 23:19:36 +0200

    #Ceng_432 books.java da attributlar eklendi.

--- app/models/Books.java
+++ app/models/Books.java
@@ -1,5 +1,20 @@
 package models;
 
 public class Books {
+	
+	private String name;
+	
+	public Books() {
+		// TODO Auto-generated constructor stub
+	}
 
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	
 }
