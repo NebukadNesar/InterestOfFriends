0db357d2a86b5dd95d827974df07d535e90a2f26
Burhan Cerit <burhancerit@gmail.com>
2012-11-25 23:19:36 +0200

    merged fiels

--- app/models/Books.java
+++ app/models/Books.java
@@ -1,12 +1,28 @@
 package models;
 
 public class Books {
+	
+	private String name;
+	
+	public Books() {
+		// TODO Auto-generated constructor stub
+	}
 
+<<<<<<< HEAD
 	
 	public Books() {
 		// TODO Auto-generated constructor stub
 	}
 	
+=======
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+>>>>>>> 9939d6f... #Ceng_432 books.java da attributlar eklendi.
 	
 	
 }
